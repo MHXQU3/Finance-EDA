5b088ab20577e2d31bf7441ce16e00c09fb1aeac
Minhaz Hoque <mi8ho7455@gmail.com>
2024-09-26 13:00:46 +0100

    Added db_utils.py

---
+++ db_utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import yaml
+from sqlalchemy import create_engine, inspect
+import psycopg2
+import os
+
+
+class RDSDatabaseConnector:
+    def __init__(self):
+        pass
